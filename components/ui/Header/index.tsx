2f3fafe42364bc1abed3ba9fd3bdb381d8b2e133
slavo3dev <slavo@slavo.io>
2023-02-16 18:26:38 -0500

    fixing

--- components/ui/Header/index.tsx
+++ components/ui/Header/index.tsx
@@ -15,11 +15,11 @@ export const Header: FC<HeaderProps> = ({ showForm, setShowForm }) =>  {
 				<img src='/logo/logo.png' height='68' width='68' alt={ appTitle } />
 			</div>
 
-			{ user?.user?.email === undefined && <button
+			{ user?.user?.email !== undefined && <button
 				className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded'
 				onClick={() => setShowForm((show: any) => !show)}
 			>
-				{showForm ? "Close" : "Ask Question"}
+				{showForm ? "Close" : "Ask Mentor a Question"}
 			</button> }
 		</header>
 	);
